468bf3c725229b5a5b9353e355c8a9209f3328b9
stevenaw <sweerdenburg@gmail.com>
2010-10-19 20:05:18 -0400

    Account for if script is not in PATH

--- handleScript.php
+++ handleScript.php
@@ -16,7 +16,7 @@
 		// It will be called c3dl.js (possibly c3dl-min.js)
 		header('Content-Disposition: attachment; filename="'.$fileName.'.js"');
 		
-		passthru("c3dlbuildbot $minify");
+		passthru("./c3dlbuildbot $minify");
 	} else
 		echo "Button not pressed";
 ?>
